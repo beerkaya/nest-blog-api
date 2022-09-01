1dd78d52ca576fc412322e3489b98b8169ec3914
beerkaya <berk-6023@yandex.com>
2022-09-01 15:14:11 +0300

    Unique decorator added

---
+++ src/utils/validator/unique.validator.ts
@@ -0,0 +1,33 @@
+import {
+    registerDecorator,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+    ValidationArguments,
+    ValidationOptions
+} from "class-validator";
+import { AppModule } from "src/app.module";
+import { EntityManager, Repository } from "typeorm";
+
+@ValidatorConstraint({async: true})
+export class UniqueConstraint implements ValidatorConstraintInterface {
+    constructor() { }
+    async validate(value: any, args: ValidationArguments) {
+        const [entity] = args.constraints;
+        const [property] = args.property.split('.');
+        const repository = new Repository<typeof entity>(entity, new EntityManager(AppModule.dataSource));
+        const count = (await repository.findBy({ [property]: value })).length;
+        return count === 0;
+    }
+}
+
+export function Unique(entity?: any, value?: string, validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: { message: `The ${propertyName} is already exists.` },
+            constraints: [entity],
+            validator: UniqueConstraint
+        });
+    };
+}
