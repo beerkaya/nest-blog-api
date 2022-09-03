ce652a235279c6d385e0bf4e28df7e66c60f410b
beerkaya <berk-6023@yandex.com>
2022-09-03 21:17:56 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppHttpModule } from './app-http.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { HttpExceptionFilter } from './http-exception.filter';
+import { EntityNotFoundFilter } from './utils/exception_filters/entity_not_found.filter';
+import { HttpExceptionFilter } from './utils/exception_filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppHttpModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new EntityNotFoundFilter);
   app.useGlobalGuards(new JwtAuthGuard(new Reflector()));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
