f3bd63c919a238a817137e75c127bf71ecb2d40a
beerkaya <berk-6023@yandex.com>
2022-09-04 18:32:38 +0300

    Jwt token expiresIn updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { UsersHttpModule } from 'src/users/users-http.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { jwtConstants } from './constants';
-import { UsersService } from 'src/users/users.service';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { JwtStrategy } from './jwt.strategy';
@@ -17,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
     Repository<User>,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1 days' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
